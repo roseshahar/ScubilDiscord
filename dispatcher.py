f786f547b3542d7fd9cdd4b478eacdc10dfb7460
Noam <noam.pnueli@gmail.com>
2017-08-28 15:30:44 +0300

    fixed being able to send commands in karma store

--- dispatcher.py
+++ dispatcher.py
@@ -71,6 +71,7 @@ async def on_message(message):
         if command not in ['ddg', 'convert', 'clear', 'buy']:
             if 'bot' not in message.channel.name:
                  await client.send_message(message.author, '"${}" is not supported in none-bot channel!'.format(command))
+                 await client.delete_message(message)
                  return
 
         args = message.content.replace('$' + command + ' ', '', 1)
